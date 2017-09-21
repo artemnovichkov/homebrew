e0a38acccb9ba06bea579161fb4e751e5306d9b2
Artem Novichkov <artemnovichkov@users.noreply.github.com>
2017-09-21 08:17:14 +0600

    Update carting.rb

--- carting.rb
+++ carting.rb
@@ -2,7 +2,7 @@ class Carting < Formula
   
   desc "🚘 Simple tool for updating Carthage script"
   homepage "https://github.com/artemnovichkov/Carting"
-  url "https://github.com/artemnovichkov/Carting/archive/1.2.7.tar.gz"
+  url "https://github.com/artemnovichkov/Carting/archive/1.2.8.tar.gz"
   sha256 "d500c1bb1b08d6037651efe3c430d12adc707aeeda1afbb3426389bc228a62b4"
 
   head 'https://github.com/artemnovichkov/Carting.git', :branch => 'master'
