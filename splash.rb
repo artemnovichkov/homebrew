e3bb040000881b0511c64fee1f15704a05198364
Artem Novichkov <artemnovichkov@users.noreply.github.com>
2017-10-23 12:55:20 +0600

    Update splash.rb

--- splash.rb
+++ splash.rb
@@ -3,7 +3,7 @@ class Splash < Formula
   desc "🚘 Simple tool for updating Carthage script"
   homepage "https://github.com/artemnovichkov/Splash"
   url "https://github.com/artemnovichkov/Splash/archive/1.0.0.tar.gz"
-  sha256 "8b09f3b1f70c6eb33c5556fe83d226517bfa5add75158844d000b751181472b4"
+  sha256 "aaf80d8ad0f5105b4d3db8e7614cb6726c96a416628fdf7451aeeffb42f7fd55"
 
   head 'https://github.com/artemnovichkov/Carting.git', :branch => 'master'
 
