e991a0947cfe41efd08998743e0282a903baf3ca
Artem Novichkov <artemnovichkov@users.noreply.github.com>
2018-01-17 09:32:29 +0600

    Update fastfood.rb

--- fastfood.rb
+++ fastfood.rb
@@ -2,7 +2,7 @@ class Fastfood < Formula
   
   desc "🍔🍟 Share Fastfile between projects"
   homepage "https://github.com/artemnovichkov/fastfood"
-  url "https://github.com/artemnovichkov/fastfood/archive/1.3.2.tar.gz"
+  url "https://github.com/artemnovichkov/fastfood/archive/1.4.tar.gz"
   sha256 "33066177e36b2f78bbb86969b18c84f39c8cc08602d9d1611bb31a477722361e"
 
   head 'https://github.com/artemnovichkov/fastfood.git', :branch => 'master'
