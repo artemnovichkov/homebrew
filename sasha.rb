c811dbfdc6b83f0f35818f8c1afa85d9e14e390a
Artem Novichkov <artemnovichkov@users.noreply.github.com>
2018-03-27 08:46:06 +0600

    Update sasha.rb

--- sasha.rb
+++ sasha.rb
@@ -2,8 +2,8 @@ class Sasha < Formula
   
   desc "👨‍💼Reduce daily routine with sasha"
   homepage "https://github.com/artemnovichkov/Sasha"
-  url "https://github.com/artemnovichkov/sasha/archive/2.1.2.tar.gz"
-  sha256 "7f0cc36ba2b202f330e84344cb7fba22e8b2eb0d7a83a5691a40a42472c760a2"
+  url "https://github.com/artemnovichkov/sasha/archive/2.1.3.tar.gz"
+  sha256 "8cc1e0443ae61de9fe86b662faa43f9cc8a324aa5601a2838dd052cc62711c40"
 
   head 'https://github.com/artemnovichkov/Sasha.git', :branch => 'master'
 
