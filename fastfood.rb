f02cfa0b5f8d978f1c0f2ca2f0ee59ae8acbf696
Artem Novichkov <novichkoff93@gmail.com>
2019-04-24 12:16:38 +0600

    Update fastfood.rb

--- fastfood.rb
+++ fastfood.rb
@@ -2,12 +2,12 @@ class Fastfood < Formula
   
   desc "🍔🍟 Share Fastfile between projects"
   homepage "https://github.com/artemnovichkov/fastfood"
-  url "https://github.com/artemnovichkov/fastfood/archive/1.4.tar.gz"
+  url "https://github.com/artemnovichkov/fastfood/archive/1.4.1.tar.gz"
   sha256 "b16d3b650970c3ad54596f43dcc53c2b59da9ef049fe9a8140a44988666de8f2"
 
   head 'https://github.com/artemnovichkov/fastfood.git', :branch => 'master'
 
-  depends_on :xcode => ["9.0", :build]
+  depends_on :xcode => ["10.0", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
