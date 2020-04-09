a2a6f5c8384d1442c39c4cd48f6fcb716155b6d6
Artem Novichkov <artem.novichkov@rosberry.com>
2020-04-09 22:12:29 +0600

    Fix spasibo formula

--- spasibo.rb
+++ spasibo.rb
@@ -1,8 +1,8 @@
 class Spasibo < Formula
   desc "Support your favourite open source frameworks"
   homepage "https://github.com/artemnovichkov/spasibo"
-  url "https://github.com/artemnovichkov/spasibo"
-  version "0.1"
+  url "https://github.com/artemnovichkov/spasibo.git", :tag => "0.1", :revision => "e4e3d8b6b9b1cab41bfa3b2dbd8153c19efe13ce"
+  head "https://github.com/artemnovichkov/spasibo.git"
 
   depends_on :xcode => ["11.0", :build]
 
