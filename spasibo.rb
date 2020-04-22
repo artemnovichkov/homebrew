ecc736abbf03ec5a93932efaf69c866deaef3904
Artem Novichkov <novichkoff93@gmail.com>
2020-04-22 22:21:55 +0600

    Update spasibo.rb

--- spasibo.rb
+++ spasibo.rb
@@ -1,7 +1,7 @@
 class Spasibo < Formula
   desc "Support your favourite open source frameworks"
   homepage "https://github.com/artemnovichkov/spasibo"
-  url "https://github.com/artemnovichkov/spasibo/archive/0.1.tar.gz"
+  url "https://github.com/artemnovichkov/spasibo/archive/0.2.tar.gz"
   sha256 "1f9fcfcfadab72d337e430ec82fa8373174b2b1c7ec55bd6d27eda5816726153"
   head "https://github.com/artemnovichkov/spasibo.git"
 
