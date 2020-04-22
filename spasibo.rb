c6eea4e28e9999530047b99446e80c55ba07359e
Artem Novichkov <novichkoff93@gmail.com>
2020-04-22 22:22:48 +0600

    Update spasibo.rb

--- spasibo.rb
+++ spasibo.rb
@@ -2,7 +2,7 @@ class Spasibo < Formula
   desc "Support your favourite open source frameworks"
   homepage "https://github.com/artemnovichkov/spasibo"
   url "https://github.com/artemnovichkov/spasibo/archive/0.2.tar.gz"
-  sha256 "1f9fcfcfadab72d337e430ec82fa8373174b2b1c7ec55bd6d27eda5816726153"
+  sha256 "3fb9bfef4d85f5a40b686d3f1d00ac7e385bc86e87f786fe1f278df1d7a3ac3c"
   head "https://github.com/artemnovichkov/spasibo.git"
 
   depends_on :xcode => ["11.0", :build]
