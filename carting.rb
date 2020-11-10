31b465458d41b172c12f4ee961d49f9ec2ddc894
Artem Novichkov <novichkoff93@gmail.com>
2020-11-10 10:32:21 +0600

    Update carting.rb

--- carting.rb
+++ carting.rb
@@ -1,11 +1,11 @@
 class Carting < Formula
   desc "🚘 Simple tool for updating Carthage scripts"
   homepage "https://github.com/artemnovichkov/Carting"
-  url "https://github.com/artemnovichkov/Carting/archive/2.1.4.tar.gz"
+  url "https://github.com/artemnovichkov/Carting/archive/2.1.5.tar.gz"
   sha256 "837d4683ce0afc1c52bb6be358a8186bf0f3c4515cde9893507075c3a57eec46"
   head "https://github.com/artemnovichkov/Carting.git"
 
-  depends_on :xcode => ["11.0", :build]
+  depends_on :xcode => ["12.0", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
